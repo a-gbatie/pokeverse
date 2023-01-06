016b6ce33c37a4a6ea73fdb15f1031de99d41f46
Ange Batie <ange.batie@gmail.com>
2023-01-05 16:34:51 -0800

    stuck on rendering img and abilities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
-const LIMIT = 150;
+const LIMIT = 5;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+    fetch(pokeApi)
+    .then(res => res.json())
+    .then((data) => {
+      setPokemonList(data.results)
+      // console.log(data)
+    })
+  }, [])
+
+  // Object.keys(pokemonList)
+  let allCards = pokemonList.map(card => (<>
+    <PokemonCard url={card.url} name={card.name}/> 
+    </>))
+  
+
   return (
+
     <div data-testid="app">
       <Navigation />
 
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <div>{allCards}</div>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
+  const [pokecard, setPokeCard] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      setPokeCard(data)
+      // console.log(data)
+      console.log(data.sprites.front_default)
+    })
+  }, [])
 
   return (
-    <div>
-        pokemon card
-    </div>
+    <>
+    <Card style={{ width: '18rem' }}>
+      {/* <Card.Img variant="top"  src={pokecard.sprites.front_default} /> */}
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {pokecard?.abilities.ability.map(powers => (
+              <ul>{powers.name}</ul>
+          ))}
+          <p>text about ldaskf la h</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </>
   );
 }
 
