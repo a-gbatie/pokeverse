397c25cb146a22a37266be6cdf0561bc7c379f66
Ange Batie <ange.batie@gmail.com>
2023-01-06 17:32:03 -0800

    working on filtering the list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-const LIMIT = 5;
+const LIMIT = 15;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
   const [pokemonList, setPokemonList] = useState([])
+  const [query, setQuery] = useState('')
+
+  console.log(pokecard.filter(pk => pk.name.includes(query)))
 
   useEffect(() => {
     fetch(pokeApi)
@@ -18,17 +23,23 @@ function App() {
     })
   }, [])
 
-  // Object.keys(pokemonList)
   let allCards = pokemonList.map(card => (<>
     <PokemonCard url={card.url} name={card.name}/> 
     </>))
+    
+  function handleSearch(){
+    console.log(hi)
+  }
   
 
   return (
 
     <div data-testid="app">
       <Navigation />
-
+      <InputGroup className="mb-3" onChange={(e) => setQuery(e.target.value)}>
+      <InputGroup.Text>Search</InputGroup.Text>
+      <Form.Control aria-label="Search" />
+      </InputGroup>
       <h1>Pokemon should appear here</h1>
       <div>{allCards}</div>
     </div>
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 function PokemonCard({ url, name }) {
   const [pokecard, setPokeCard] = useState([])
 
+
   useEffect(() => {
     fetch(url)
     .then(res => res.json())
@@ -14,22 +15,26 @@ function PokemonCard({ url, name }) {
     })
   }, [])
 
+  const getAbility = pokecard?.abilities?.map(powers => (
+      <ul><li>{powers.ability.name}</li></ul>
+      )) 
+
+  
+
   return (
     <>
     <Card style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top"  src={pokecard.sprites.front_default} /> */}
+      <Card.Img variant="top"  src={pokecard?.sprites?.front_default} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          {pokecard?.abilities.ability.map(powers => (
-              <ul>{powers.name}</ul>
-          ))}
-          <p>text about ldaskf la h</p>
+        <Card.Text> Abilities:
+          {getAbility}
         </Card.Text>
       </Card.Body>
     </Card>
     </>
   );
+  
 }
 
 export { PokemonCard };
