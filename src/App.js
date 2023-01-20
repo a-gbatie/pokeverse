aa5eb32b87c3dcf3b1f201e191a62b37993ceeb3
Ange Batie <ange.batie@gmail.com>
2023-01-19 17:00:41 -0800

    got all cards showing, filter working, grided

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const LIMIT = 15;
+const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
   const [pokemonList, setPokemonList] = useState([])
   const [query, setQuery] = useState('')
-
-  console.log(pokecard.filter(pk => pk.name.includes(query)))
 
   useEffect(() => {
     fetch(pokeApi)
@@ -23,27 +24,38 @@ function App() {
     })
   }, [])
 
-  let allCards = pokemonList.map(card => (<>
-    <PokemonCard url={card.url} name={card.name}/> 
-    </>))
+  // let allCards = pokemonList.map(card => (<>
+  //   <PokemonCard url={card.url} name={card.name}/> 
+  //   </>))
     
-  function handleSearch(){
-    console.log(hi)
+  function handleSearch(e){
+    setQuery(e.target.value)
+    // console.log(hi)
   }
   
 
   return (
-
-    <div data-testid="app">
-      <Navigation />
-      <InputGroup className="mb-3" onChange={(e) => setQuery(e.target.value)}>
-      <InputGroup.Text>Search</InputGroup.Text>
-      <Form.Control aria-label="Search" />
-      </InputGroup>
-      <h1>Pokemon should appear here</h1>
-      <div>{allCards}</div>
-    </div>
+    <Container>
+      <div data-testid="app">
+        <Navigation />
+        <InputGroup className="mb-3" >
+        <InputGroup.Text>Search</InputGroup.Text>
+        <Form.Control aria-label="Search" onChange={handleSearch} />
+        </InputGroup>
+        <h1>Pokemon should appear here</h1>
+        {/* <div>{allCards}</div> */}
+        <Row>
+          {pokemonList.filter(card => card.name.toLowerCase().includes(query.toLowerCase())).map(card => (
+            <Col>
+              <PokemonCard key={card.name} url={card.url} name={card.name} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </Container>
   );
 }
+
+
 
 export { App };
